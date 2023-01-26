5df4121961afbb3613b7314b868471f9a12ec94f
Jonatan Hanssen <jonatan.hanssen@outlook.com>
2023-01-26 11:46:03 +0100

    Fixed before mazunki
    Signed-off-by: Jonatan Hanssen <jonatan.hanssen@outlook.com>

--- eduroam-connection.sh
+++ eduroam-connection.sh
@@ -2,6 +2,7 @@
 # sets up eduroam with username
 # use nmcli connection up eduroam --ask to give password afterwards
 
+card="wlp0s20f3"
 username="jonatahh@uio.no"
-nmcli connection add type wifi con-name "eduroam" ifname wlo1 ssid "eduroam" -- wifi-sec.key-mgmt wpa-eap 802-1x.eap ttls 802-1x.phase2-auth mschapv2 802-1x.identity "$username"
+nmcli connection add type wifi con-name "eduroam" ifname "$card" ssid "eduroam" -- wifi-sec.key-mgmt wpa-eap 802-1x.eap ttls 802-1x.phase2-auth mschapv2 802-1x.identity "$username"
 
