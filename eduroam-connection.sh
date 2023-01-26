8e2416dabb3d74c38450bdc780e8b3ec77075685
Mazunki Hoksaas <rolferen@gmail.com>
2023-01-26 11:51:47 +0100

    Posix is cool

--- eduroam-connection.sh
+++ eduroam-connection.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
 # sets up eduroam with username
 # use nmcli connection up eduroam --ask to give password afterwards
+set -e
+
+interface="wlo1"
+conname="eduroam"
 
 username="jonatahh@uio.no"
-nmcli connection add type wifi con-name "eduroam" ifname wlo1 ssid "eduroam" -- wifi-sec.key-mgmt wpa-eap 802-1x.eap ttls 802-1x.phase2-auth mschapv2 802-1x.identity "$username"
+ssid="eduroam"
+
+nmcli connection add \
+	type wifi \
+	con-name "${connname:?}" \
+	ifname "${interface:?Must provide interface}" \
+	ssid "${ssid:?Must provide ssid}" -- \
+		wifi-sec.key-mgmt wpa-eap \
+		802-1x.eap ttls \
+		802-1x.phase2-auth mschapv2 \
+		802-1x.identity "${username:?Must provide username}"
+
+printf "You've succesfully created the connection for %s with username %s\n" "${ssid}" "${username}"
+printf "To continue, run the following command, and type the password:\n\tnmcli connection up %s --ask\n" "${connname}"
 
